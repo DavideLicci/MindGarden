d97eacb1fbe505f3c11cebca2c9dafe6c5a14930
MindGarden Dev <dev@local>
2025-11-27 12:55:29 +0100

    prova con git

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   appDir: 'frontend/app',
-  experimental: {
-    appDir: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
   },
 };
 
